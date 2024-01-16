56efc12c9cae3c963289cc7ed6a45939ecd7afac
YuraMatyash <YuraMatyash@git.com>
2024-01-16 21:42:31 +0300

    datatime izmen

--- усложнение умолчания.py
+++ усложнение умолчания.py
@@ -2,7 +2,7 @@ import copy
 from datetime import date
 
 def get_weekday():
-    return date.today().strftime('%A')
+    return date.today().strftime('%Y-%m-%d')
 
 def get_month(post, weekday=get_weekday()):
     post_copy = post.copy()
@@ -16,3 +16,4 @@ instal_post = {
 }
 instal_post = get_month(instal_post)
 print(instal_post)
+
