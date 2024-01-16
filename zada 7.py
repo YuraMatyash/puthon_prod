4947ab45ba6952c89f0aae657bf7a1f4d9becaea
YuraMatyash <YuraMatyash@git.com>
2024-01-16 17:05:02 +0300

    main

---
+++ zada 7.py
@@ -0,0 +1,8 @@
+def merge_lists_to_dict(**arg):
+    print(arg)
+    my_zip = arg['list1']
+    my_dict = arg['list2']
+    return my_zip,my_dict
+# если так задавать то только квадратные скобки
+print(merge_lists_to_dict(list1=[1,2,3],list2=[4,5,5]))
+
