2d92e5ed4b7a5e8e4c202982424a2a0602f89a04
YuraMatyash <YuraMatyash@git.com>
2024-01-29 20:21:51 +0300

    apdate

--- задача 15 циклы.py
+++ задача 15 циклы.py
@@ -1,13 +1,14 @@
-
-try:
-    while True:
+while True:
+     try:
         nun1 = float(input('введите первое число: '))
         nun2 = float(input('введите второе число: '))
-        print(nun1 / nun2)
-        wish = input('хотите продолжить yes/no: ')
-        if wish == 'yes':
-             continue
-        if wish == 'no':
-             break
-except Exception as e:
-    print(e)
+     except Exception as e:
+        print(e)
+        continue
+
+     print(nun1 / nun2)
+     wish = input('хотите продолжить yes/no: ')
+     if wish == 'yes':
+        continue
+     if wish == 'no':
+        break
